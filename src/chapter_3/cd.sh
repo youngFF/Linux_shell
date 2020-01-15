2a25a8f1d9b51a698fd4484f4aa2753993c37d97
yangxingdong <yangxingdong@jd.com>
2020-01-15 15:09:27 +0800

    1。第三章看完 2。command line这个概念 3。tail -f 4。-T，-t 命令选项大小写的关系

--- src/chapter_3/cd.sh
+++ src/chapter_3/cd.sh
@@ -6,3 +6,14 @@ ls -aF
 ls -aR
 ls -ar
 ls -l
+
+# 模糊
+# ? 代表一个字符
+# * 代表一个或多个字符
+
+ls -l my_script  #匹配my_script
+ls -l my?script     #匹配 myascript ,mybscript 等
+ls -l my*script     #匹配 myscript,myascript,myaaaascript 等
+ls -l my[ai]script  #匹配 myascript , myiscript
+ls -l my[a-i]script # 匹配 myascript , mybscript,mycscript......myiscript
+
